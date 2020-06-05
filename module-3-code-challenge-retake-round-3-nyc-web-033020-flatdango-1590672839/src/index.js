9e15ff7c276d4bf1f32c2b31e6fe43a7bce43074
Yahyagok <ygok17@hotmail.com>
2020-06-05 17:51:33 -0400

    new

---
+++ module-3-code-challenge-retake-round-3-nyc-web-033020-flatdango-1590672839/src/index.js
@@ -0,0 +1,108 @@
+const url = "http://localhost:3000/films"
+const filmsDiv = () => document.querySelector("#films")
+
+const posterDiv = () => document.querySelector("#poster")
+const showingDiv = () => document.querySelector("#showing")
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+const posterFirst = document.getElementById('poster')
+const titleFirst = document.getElementById('title')
+const runtimeFirst = document.getElementById('runtime')
+const filmInfo = document.getElementById('film-info')
+const showTime = document.getElementById('showtime')
+const ticketNum = document.getElementById('ticket-num')
+const button = document.getElementsByClassName('ui orange button')[0]
+// const extraContent = document.getElementsByClassName('extra content')[0]
+const card = document.getElementsByClassName('card')[0]
+const div = document.createElement('div')
+
+const filmItem = document.getElementsByClassName('film item')[0]
+
+
+card.append(div)
+
+    firstMovie()
+    function firstMovie(){
+        fetch(`http://localhost:3000/films/1`)
+        .then(r => r.json())
+        .then(movie => getData(movie))
+    }
+
+    function getData(movie){
+        posterFirst.src = movie.poster
+        titleFirst.innerHTML = movie.title
+        runtimeFirst.innerHTML = `${movie.runtime} minutes`
+        filmInfo.innerHTML = movie.description
+        showTime.innerHTML = movie.showtime
+
+        div.innerHTML = movie.tickets_sold
+
+        ticketNum.innerHTML = `${parseInt(movie.capacity)}` - `${movie.tickets_sold}`
+  
+    }
+
+    // when you click on buy tickets , remaining tickest will decrease 
+    document.addEventListener('click', function(event){
+        if (event.target.className === 'ui orange button'){
+           // when user clicks on button 
+           // I wnat titckets sold get increase 
+           // for this reason x will get decrease because of minus thing 
+            // div.innerHTML++
+
+          
+           fetch(`http://localhost:3000/films/1`)
+           .then( r => r.json())
+           .then(firstMovie => {
+                 let x =  firstMovie.tickets_sold
+                 x++
+                 let y = parseInt(firstMovie.capacity) 
+                 ticketNum.innerHTML = y - x
+
+
+                fetch(`http://localhost:3000/films/1`, {
+                    method: "PATCH",
+                    headers:   {
+                        'Content-Type': 'application/json',
+                        'accept': 'application/json'
+                      },
+                      body: JSON.stringify({ tickets_sold: x }) 
+                })
+
+                if (x > y ){ 
+               let  x = y 
+               ticketNum.innerHTML = `0 no more tickets` 
+                    fetch(`http://localhost:3000/films/1`, {
+                    method: "PATCH",
+                    headers:   {
+                        'Content-Type': 'application/json',
+                        'accept': 'application/json'
+                      },
+                      body: JSON.stringify({ tickets_sold: x }) 
+                     })
+                   }
+                })                      
+               }  
+     })
+
+             allMovies()
+               function allMovies(){
+                   fetch('http://localhost:3000/films')
+                   .then(r => r.json())
+                   .then(movies => getMovies(movies))
+               }
+               function getMovies(movies){
+                    movies.forEach(movie=> {
+                        const li = document.createElement('li')
+                        li.innerHTML = movie.title 
+                        filmItem.append(li)
+                    })        
+               }
+});
+
+
+
+
+
+// const totalSold = document.getElementById('total-sold')
+// console.log(totalSold)
